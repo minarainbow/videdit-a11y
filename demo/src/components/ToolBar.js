7cf528df4b777b56e5a82cb3309128daf48db22c
Saelyne <saelyne@kaist.ac.kr>
2022-04-28 23:46:21 +0900

    jump on selected text

--- demo/src/components/ToolBar.js
+++ demo/src/components/ToolBar.js
@@ -9,6 +9,7 @@ import ContentCutIcon from "@mui/icons-material/ContentCut";
 import AddCommentIcon from "@mui/icons-material/AddComment";
 import TextField from "@mui/material/TextField";
 import { FormControlUnstyledContext } from "@mui/base";
+import { child } from "firebase/database";
 
 const results = [
   {
@@ -35,6 +36,8 @@ class ToolBar extends React.Component {
       activeItem: null,
       rangeClick: false,
       invalidRange: false,
+      trimStartTime: "",
+      trimEndTime: "",
     };
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.timeoutRef = React.createRef();
@@ -117,10 +120,6 @@ class ToolBar extends React.Component {
     }
   };
 
-  handleTrimClick = () => {
-    this.setState({ rangeClick: !this.state.rangeClick });
-  };
-
   formatTime(time) {
     const integer = Math.floor(time);
     const underSecs = (time - integer).toFixed(2).toString().substring(2);
@@ -159,61 +158,94 @@ class ToolBar extends React.Component {
     }
   };
 
+  handleTrimClick = () => {
+    const [startDiv, endDiv] = this.props.selectedDivs;
+    const startTime = startDiv
+      .querySelectorAll("span.Word")[0]
+      .getAttribute("data-start");
+    const divs = endDiv.querySelectorAll("span.Word");
+    const endTime = divs[divs.length - 1].getAttribute("data-end");
+    this.setState({
+      rangeClick: !this.state.rangeClick,
+      trimStartTime: startTime,
+      trimEndTime: endTime,
+    });
+  };
+
   onClickTrim = () => {
-    const currSpan = document.querySelector(
-      `span.Word[data-start="${this.props.currWordStart}"]`
+    // trimStartTime;
+    const [startDiv, endDiv] = this.props.selectedDivs;
+    const defaultStartTime = parseFloat(
+      startDiv
+        .querySelectorAll("span.Word")[0]
+        .getAttribute("data-default-start")
     );
-    if (currSpan) {
-      const defaultStartTime = parseFloat(
-        currSpan.getAttribute("data-default-start")
-      );
-      const defaultEndTime = parseFloat(
-        currSpan.getAttribute("data-default-end")
-      );
-      const oldStartTime = parseFloat(currSpan.getAttribute("data-start"));
-      const oldEndTime = parseFloat(currSpan.getAttribute("data-end"));
-      const startTime = this.calculateTime(this.startRef.current.value);
-      const endTime = this.calculateTime(this.endRef.current.value);
-      console.log(oldStartTime, oldEndTime, startTime, endTime);
-      if (startTime < defaultStartTime || endTime > defaultEndTime) {
-        console.log("invalid");
-        this.setState({ invalidRange: true });
-      } else {
-        if (startTime !== oldStartTime) {
-          currSpan.setAttribute("data-start", startTime);
-          currSpan.setAttribute("trim-start", "true");
-        }
-        if (endTime !== oldEndTime) {
-          currSpan.setAttribute("data-end", endTime);
-          currSpan.setAttribute("trim-end", "true");
-        }
-        this.setState({ rangeClick: !this.state.rangeClick });
-        this.props.onStartPlay();
+    const divs = endDiv.querySelectorAll("span.Word");
+    const defaultEndTime = parseFloat(
+      divs[divs.length - 1].getAttribute("data-default-end")
+    );
+    const oldStartTime = this.state.trimStartTime;
+    const oldEndTime = this.state.trimEndTime;
+    const startTime = this.calculateTime(this.startRef.current.value);
+    const endTime = this.calculateTime(this.endRef.current.value);
+    if (startTime < defaultStartTime || endTime > defaultEndTime) {
+      this.setState({ invalidRange: true });
+    } else {
+      if (startTime !== oldStartTime) {
+        startDiv
+          .querySelectorAll("span.Word")[0]
+          .setAttribute("data-start", startTime);
+        startDiv
+          .querySelectorAll("span.Word")[0]
+          .setAttribute("trim-start", "true");
       }
+      if (endTime !== oldEndTime) {
+        divs[divs.length - 1].setAttribute("data-end", endTime);
+        divs[divs.length - 1].setAttribute("trim-end", "true");
+      }
+      this.setState({ rangeClick: !this.state.rangeClick });
+      this.props.onStartPlay();
     }
   };
 
   handleChangeSpeed = (rate) => {
-    const currSpan = document.querySelector(
-      `span.Word[data-start="${this.props.currWordStart}"]`
-    );
-    console.log(currSpan);
-    if (currSpan) {
-      currSpan.setAttribute("data-playback", rate);
+    const [startDiv, endDiv] = this.props.selectedDivs;
+    const startKey = startDiv.getAttribute("data-offset-key");
+    const endKey = endDiv.getAttribute("data-offset-key");
+    const children = startDiv.parentElement.children;
+    var i = 0;
+    var startIdx;
+    for (i = 0; i < children.length; i++) {
+      if (startKey === children[i].getAttribute("data-offset-key")) {
+        startIdx = i;
+        break;
+      }
+    }
+    for (i = startIdx; i < children.length; i++) {
+      var words = children[i].querySelectorAll("span.Word");
+      for (var j = 0; j < words.length; j++) {
+        words[j].setAttribute("data-playback", rate);
+      }
+      if (endKey === children[i].getAttribute("data-offset-key")) {
+        break;
+      }
     }
   };
 
   render() {
     const { loading, results, value } = this.state;
-    console.log(this.props.selectedDivs)
     const resRender = ({ type, keyword }) => (
       <span key="keyword">
         {type}: {keyword}
       </span>
     );
     return (
-      <div className="tool-bar" onMouseDown={(event) => {
-        event.preventDefault();}}>
+      <div
+        className="tool-bar"
+        onMouseDown={(event) => {
+          event.preventDefault();
+        }}
+      >
         <Menu icon="labeled" className="tool-icon">
           <Menu.Item name="comment" onClick={this.handleAddComment}>
             <AddCommentIcon style={{ fontSize: "30px" }} />
@@ -222,45 +254,55 @@ class ToolBar extends React.Component {
           <Dropdown
             icon={null}
             trigger={
-              <div onMouseDown={(event) => {
-                event.preventDefault();
-                this.handleItemClick();
-              }}>
-              <Menu.Item name="speed" onClick={this.handleItemClick}>
-                <SpeedIcon style={{ fontSize: "30px" }} />
-                Speed
-              </Menu.Item>
+              <div
+                onMouseDown={(event) => {
+                  event.preventDefault();
+                  this.handleItemClick();
+                }}
+              >
+                <Menu.Item name="speed" onClick={this.handleItemClick}>
+                  <SpeedIcon style={{ fontSize: "30px" }} />
+                  Speed
+                </Menu.Item>
               </div>
             }
           >
             <Dropdown.Menu vertical>
-              <div onMouseDown={(event) => {
-                event.preventDefault();
-              }}>
+              <div
+              // onMouseDown={(event) => {
+              //   event.preventDefault();
+              // }}
+              >
                 <Dropdown.Item
                   text="x 0.5"
                   onClick={() => this.handleChangeSpeed(0.5)}
                 />
               </div>
-              <div onMouseDown={(event) => {
-                event.preventDefault();
-              }}>
+              <div
+              // onMouseDown={(event) => {
+              //   event.preventDefault();
+              // }}
+              >
                 <Dropdown.Item
                   text="x 1.0"
                   onClick={() => this.handleChangeSpeed(1.0)}
                 />
               </div>
-              <div onMouseDown={(event) => {
-                event.preventDefault();
-              }}>
+              <div
+              // onMouseDown={(event) => {
+              //   event.preventDefault();
+              // }}
+              >
                 <Dropdown.Item
                   text="x 1.5"
                   onClick={() => this.handleChangeSpeed(1.5)}
                 />
               </div>
-              <div onMouseDown={(event) => {
-                event.preventDefault();
-              }}>
+              <div
+              // onMouseDown={(event) => {
+              //   event.preventDefault();
+              // }}
+              >
                 <Dropdown.Item
                   text="x 2.0"
                   onClick={() => this.handleChangeSpeed(2.0)}
@@ -271,9 +313,11 @@ class ToolBar extends React.Component {
           <Dropdown
             icon={null}
             trigger={
-              <div onMouseDown={(event) => {
-                event.preventDefault();
-              }}>
+              <div
+                onMouseDown={(event) => {
+                  event.preventDefault();
+                }}
+              >
                 <Menu.Item name="trim" onClick={this.handleTrimClick}>
                   <ContentCutIcon style={{ fontSize: "30px" }} />
                   Trim
@@ -293,7 +337,7 @@ class ToolBar extends React.Component {
                     shrink: true,
                   }}
                   variant="filled"
-                  defaultValue={this.formatTime(this.props.currWordStart)}
+                  defaultValue={this.formatTime(this.state.trimStartTime)}
                   size="small"
                   style={{ width: "100px" }}
                   inputRef={this.startRef}
@@ -305,7 +349,7 @@ class ToolBar extends React.Component {
                     shrink: true,
                   }}
                   variant="filled"
-                  defaultValue={this.formatTime(this.props.currWordEnd)}
+                  defaultValue={this.formatTime(this.state.trimEndTime)}
                   size="small"
                   style={{ width: "100px" }}
                   inputRef={this.endRef}
