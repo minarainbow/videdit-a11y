4146ccd34698e0339c19389ddf3adf34fa678fa9
Saelyne <saelyne@kaist.ac.kr>
2022-05-27 14:54:16 +0900

    speed change reflected in rendered videos

--- deact/extract/render_video.py
+++ deact/extract/render_video.py
@@ -17,7 +17,8 @@ for sentence in soup.find_all('div', {'class' : 'sentence'}):
     for word in sentence.find_all('span',  {"data-start" : True}):
         start = word["data-start"]
         end = word["data-end"]
-        timestamps.append((start, end))
+        speed = word["data-playback"]
+        timestamps.append((start, end, speed))
 # print (timestamps)
 
 # Merge timestamps
@@ -26,13 +27,14 @@ new = True
 for i in range (len(timestamps)-1):
     if new:
         start = timestamps[i][0]
-    if timestamps[i][1] == timestamps[i+1][0]:
+        speed = timestamps[i][2]
+    if timestamps[i][1] == timestamps[i+1][0] and timestamps[i][2] == timestamps[i+1][2]:
         end = timestamps[i+1][1]
         new = False
     else:
-        merged_timestamps.append((start, end))
+        merged_timestamps.append((start, end, speed))
         new = True
-merged_timestamps.append((start, timestamps[-1][1]))
+merged_timestamps.append((start, timestamps[-1][1], timestamps[-1][2]))
 # print (merged_timestamps)
 
 # Cut and merge videos according to merged_timestamps
@@ -41,6 +43,7 @@ merged_timestamps.append((start, timestamps[-1][1]))
 clips = []
 for segment in merged_timestamps:
     clip = original_video.subclip(float(segment[0]), float(segment[1]))
+    clip = clip.fx(vfx.speedx, float(segment[2]))
     clips.append(clip)
 final = concatenate_videoclips(clips)
 #writing the video into a file / saving the combined video
