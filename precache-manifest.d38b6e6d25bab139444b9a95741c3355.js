0b6e3802ecd932159e7d7885c7bb90f7cc5b6394
minarainbow <minarainbow@kaist.ac.kr>
2022-06-25 14:19:51 +0900

    Updates

---
+++ precache-manifest.d38b6e6d25bab139444b9a95741c3355.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8981bda58df51a767f",
+    "url": "/videdit-a11y/static/css/main.b286b805.chunk.css"
+  },
+  {
+    "revision": "ca8981bda58df51a767f",
+    "url": "/videdit-a11y/static/js/main.9b188020.chunk.js"
+  },
+  {
+    "revision": "fba798babcb1700f370a",
+    "url": "/videdit-a11y/static/js/runtime~main.80c12aab.js"
+  },
+  {
+    "revision": "89fd9115510944b5ca12",
+    "url": "/videdit-a11y/static/css/2.9a348c1f.chunk.css"
+  },
+  {
+    "revision": "89fd9115510944b5ca12",
+    "url": "/videdit-a11y/static/js/2.7105706d.chunk.js"
+  },
+  {
+    "revision": "458427526eab503a65a7",
+    "url": "/videdit-a11y/static/js/3.4daa29c8.chunk.js"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/videdit-a11y/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/videdit-a11y/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/videdit-a11y/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/videdit-a11y/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/videdit-a11y/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/videdit-a11y/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/videdit-a11y/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/videdit-a11y/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/videdit-a11y/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/videdit-a11y/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/videdit-a11y/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/videdit-a11y/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/videdit-a11y/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/videdit-a11y/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/videdit-a11y/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/videdit-a11y/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e1a0256db8c5077622a8197b9ecb99e5",
+    "url": "/videdit-a11y/index.html"
+  }
+];
